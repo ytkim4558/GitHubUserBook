b5755a4f592ac537901b9eeda75f3551cea89009
ytkim4558 <ytkim4558@naver.com>
2018-05-19 13:19:02 +0900

    탭 생성, 리스트뷰 생성

--- app/src/main/java/com/example/ytkim/githubuserbook/MainActivity.java
+++ app/src/main/java/com/example/ytkim/githubuserbook/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.ytkim.githubuserbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabHost tabHost;
+
+    double Official = 3.305785;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("1").setContent(R.id.tab1).setIndicator("User");
+
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("2").setContent(R.id.tab2).setIndicator("Like User");
+
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
     }
 }
